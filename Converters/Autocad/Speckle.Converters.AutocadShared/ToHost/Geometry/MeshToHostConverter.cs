60e90b753af0a8a2025a93d0d41c0041a16fb0f1
Solenn Le Pense <63291453+lepenss@users.noreply.github.com>
2025-10-08 10:37:05 +0100

    converts mesh to solid on import from Speckle to Civil3D

--- Converters/Autocad/Speckle.Converters.AutocadShared/ToHost/Geometry/MeshToHostConverter.cs
+++ Converters/Autocad/Speckle.Converters.AutocadShared/ToHost/Geometry/MeshToHostConverter.cs
@@ -1,13 +1,17 @@
 using Speckle.Converters.Common;
 using Speckle.Converters.Common.Objects;
 using Speckle.Objects.Utils;
-using Speckle.Sdk;
+//using Speckle.Sdk;
 using Speckle.Sdk.Models;
+
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.Geometry;
 
 namespace Speckle.Converters.Autocad.Geometry;
 
+
 [NameAndRankValue(typeof(SOG.Mesh), NameAndRankValueAttribute.SPECKLE_DEFAULT_RANK)]
-public class MeshToHostConverter : IToHostTopLevelConverter, ITypedConverter<SOG.Mesh, ADB.PolyFaceMesh>
+public class MeshToHostConverter : IToHostTopLevelConverter, ITypedConverter<SOG.Mesh, ADB.Solid3d>
 {
   private readonly ITypedConverter<SOG.Point, AG.Point3d> _pointConverter;
   private readonly IConverterSettingsStore<AutocadConversionSettings> _settingsStore;
@@ -23,10 +27,13 @@ public class MeshToHostConverter : IToHostTopLevelConverter, ITypedConverter<SOG
 
   public object Convert(Base target) => Convert((SOG.Mesh)target);
 
+
+
+
   /// <remarks>
   /// Mesh conversion requires transaction since it's vertices needed to be added into database in advance..
   /// </remarks>
-  public ADB.PolyFaceMesh Convert(SOG.Mesh target)
+  public ADB.Solid3d Convert(SOG.Mesh target)
   {
     target.TriangulateMesh(true);
 
@@ -38,79 +45,79 @@ public class MeshToHostConverter : IToHostTopLevelConverter, ITypedConverter<SOG
       vertices.Add(point);
     }
 
-    ADB.PolyFaceMesh mesh = new();
-
-    //TODO using?
     ADB.Transaction tr = _settingsStore.Current.Document.TransactionManager.TopTransaction;
-
-    mesh.SetDatabaseDefaults();
 
     // append mesh to blocktable record - necessary before adding vertices and faces
     var btr = (ADB.BlockTableRecord)
-      tr.GetObject(_settingsStore.Current.Document.Database.CurrentSpaceId, ADB.OpenMode.ForWrite);
-    btr.AppendEntity(mesh);
-    tr.AddNewlyCreatedDBObject(mesh, true);
+    tr.GetObject(_settingsStore.Current.Document.Database.CurrentSpaceId, ADB.OpenMode.ForWrite);
 
-    // add polyfacemesh vertices
-    for (int i = 0; i < vertices.Count; i++)
-    {
-      var vertex = new ADB.PolyFaceMeshVertex(points[i]);
-      if (i < target.colors.Count)
-      {
-        try
-        {
-          if (System.Drawing.Color.FromArgb(target.colors[i]) is System.Drawing.Color color)
-          {
-            vertex.Color = Autodesk.AutoCAD.Colors.Color.FromRgb(color.R, color.G, color.B);
-          }
-        }
-        catch (System.Exception e) when (!e.IsFatal())
-        {
-          // POC: should we warn user?
-          // Couldn't set vertex color, but this should not prevent conversion.
-        }
-      }
 
-      if (vertex.IsNewObject)
-      {
-        mesh.AppendVertex(vertex);
-        tr.AddNewlyCreatedDBObject(vertex, true);
-      }
-    }
+    // Create surfaces from mesh faces
+    DBObjectCollection surfaces = new DBObjectCollection();
 
-    // add polyfacemesh faces. vertex index starts at 1 sigh
+
     int j = 0;
     while (j < target.faces.Count)
     {
-      ADB.FaceRecord face;
+      AG.Point3d p1, p2, p3, p4;
       if (target.faces[j] == 3) // triangle
       {
-        face = new ADB.FaceRecord(
-          (short)(target.faces[j + 1] + 1),
-          (short)(target.faces[j + 2] + 1),
-          (short)(target.faces[j + 3] + 1),
-          0
-        );
+        p1 = points[target.faces[j + 1]];
+        p2 = points[target.faces[j + 2]];
+        p3 = points[target.faces[j + 3]];
+
+        using (var f = new Autodesk.AutoCAD.DatabaseServices.Face())
+        {
+          // A 3DFace always has 4 vertices; for a triangle, repeat the 3rd point
+          f.SetVertexAt(0, p1);
+          f.SetVertexAt(1, p2);
+          f.SetVertexAt(2, p3);
+          f.SetVertexAt(3, p3); // duplicate for triangular face
+                                // Alternatively:
+                                // using var f = new Face(p1, p2, p3, p3, true, true, true, true);
+
+          var s = Autodesk.AutoCAD.DatabaseServices.Surface.CreateFrom(f);
+           surfaces.Add(s);
+
+          
+        }
+
         j += 4;
       }
       else // quad
       {
-        face = new ADB.FaceRecord(
-          (short)(target.faces[j + 1] + 1),
-          (short)(target.faces[j + 2] + 1),
-          (short)(target.faces[j + 3] + 1),
-          (short)(target.faces[j + 4] + 1)
-        );
-        j += 5;
-      }
+        p1 = points[target.faces[j + 1]];
+        p2 = points[target.faces[j + 2]];
+        p3 = points[target.faces[j + 3]];
+        p4 = points[target.faces[j + 4]];
 
-      if (face.IsNewObject)
-      {
-        mesh.AppendFaceRecord(face);
-        tr.AddNewlyCreatedDBObject(face, true);
+
+        using (var f = new Autodesk.AutoCAD.DatabaseServices.Face(p1, p2, p3, p4, true, true, true, true))
+        {
+          var s = Autodesk.AutoCAD.DatabaseServices.Surface.CreateFrom(f);
+          surfaces.Add(s);
+
+            
+        }
+
+
+        j += 5;
       }
     }
 
-    return mesh;
+
+    // Create solid from surfaces
+    ADB.Solid3d solid = new ADB.Solid3d();
+    solid.SetDatabaseDefaults();
+
+    var flags = new IntegerCollection(); // can be empty
+    solid.CreateSculptedSolid(surfaces.Cast<Autodesk.AutoCAD.DatabaseServices.Entity>().ToArray(), flags);
+
+    btr.AppendEntity(solid);
+    tr.AddNewlyCreatedDBObject(solid, true);
+
+
+    return solid;
+
   }
 }
